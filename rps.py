3b9f7a0d4eef7b7f258bfcbb8d8d276b21871930
shmiko <shmiko@gmail.com>
2016-08-27 20:23:28 +1000

    RPS complete

--- rps.py
+++ rps.py
@@ -36,4 +36,40 @@ def move():
 			pass
 		print "Oops! I didn't understand that. Please enter 1,2 or 3."
 
-		pass
+def result(player,computer):
+	print ("1...")
+	time.sleep(1)
+	print ("2...")
+	time.sleep(1)
+	print ("3...")
+	time.sleep(0.5)
+	print ("Computer threw (0)!".format(names(computer)))
+	global player_score, computer_score
+	if player == computer:
+		print ("Tie game.")
+	else:
+		if rules[player] == computer:
+			print ("Your victory has been assured.")
+			player_score += 1
+		else:
+			print ("The computer laughs as you realise you have been defeated.")
+			computer_score += 1
+
+def play_again():
+	answer = raw_input("Would you like to play again? y/n: ")
+	if answer in ("y", "Y", "yes", "Yes", "Of course"):
+		return answer
+	else:
+		print ("Thank you very much for playing our game. See you next time!")
+
+def scores():
+	global player_score, computer_score
+	print ("HIGH SCORES")
+	print ("Player: ", player_score)
+	print ("Computer: ", computer_score)
+
+if __name__ == '__main__':
+	start()
+
+
+
