f9c7576f8122ddabc531c50bfa73e190876a4b21
shmiko <shmiko@gmail.com>
2016-08-27 20:03:19 +1000

    RPS functions

--- rps.py
+++ rps.py
@@ -11,3 +11,29 @@ rules = { rock: scissors, paper: rock, scissors: paper}
 
 player_score = 0
 computer_score = 0
+
+def start():
+	print ("Lets play a game of Rock, Paper, Scissors.")
+	while game():
+		pass
+	scores()
+
+def game():
+	player = move()
+	computer = random.randint(1,3)
+	result(player,computer)
+	return play_again()
+
+def move():
+	while True:
+		print
+		player = raw_input("Rock = 1\nPaper = 2\nScissors = 3\nMake a move: ")
+		try:
+			player = int(player)
+			if player in (1,2,3):
+				return player
+		except ValueError:
+			pass
+		print "Oops! I didn't understand that. Please enter 1,2 or 3."
+
+		pass
