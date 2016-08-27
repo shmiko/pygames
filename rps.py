c8fa0d548110dd9a72153bd722ba20e5def53f2c
shmiko <shmiko@gmail.com>
2016-08-27 19:50:52 +1000

    vars RPS

--- rps.py
+++ rps.py
@@ -5,3 +5,9 @@ import time
 rock = 1
 paper = 2
 scissors = 3
+
+names = { rock: "Rock", paper: "Paper", scissors: "Scissors"}
+rules = { rock: scissors, paper: rock, scissors: paper}
+
+player_score = 0
+computer_score = 0
